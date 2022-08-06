245918686d8dd3fb2fbf2016bf41e5dd8f702e59
daisnurfaizi <dsnurfaizi@gmail.com>
2022-08-06 23:13:18 +0700

    add view product

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ var cors = require('cors');
 var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
-// app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
